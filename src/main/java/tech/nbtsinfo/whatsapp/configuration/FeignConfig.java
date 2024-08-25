cbed16e9bc21f05abb1bcea8eca1600e845d913a
Vinicius Norberto <vpereira.norberto@gmail.com>
2024-08-25 17:31:16 -0300

    Cria configuracao para log no feign

---
+++ src/main/java/tech/nbtsinfo/whatsapp/configuration/FeignConfig.java
@@ -0,0 +1,12 @@
+package tech.nbtsinfo.whatsapp.configuration;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+
+public class FeignConfig {
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+}
