d4a37743d5d75ebfd52abf0a953580a7e7bef858
Vinicius Norberto <vpereira.norberto@gmail.com>
2024-08-25 17:28:23 -0300

    Cria camada Controller

--- src/main/java/tech/nbtsinfo/whatsapp/controller/WebhookController.java
+++ src/main/java/tech/nbtsinfo/whatsapp/controller/WebhookController.java
@@ -2,20 +2,27 @@ package tech.nbtsinfo.whatsapp.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import tech.nbtsinfo.whatsapp.model.WebhookRequest;
+import tech.nbtsinfo.whatsapp.model.WebhookEvolution;
+import tech.nbtsinfo.whatsapp.parameter.WebhookEvolutionParameter;
 import tech.nbtsinfo.whatsapp.service.WhatsAppService;
 
 @RestController
-@RequestMapping("/webhook")
+@RequestMapping("/webhooks")
 public class WebhookController {
 
     @Autowired
     private WhatsAppService whatsAppService;
 
     @PostMapping("/messages-upsert")
-    public ResponseEntity<?> handleMessage(@RequestBody WebhookRequest request) {
-        return ResponseEntity.ok(whatsAppService.handleMessage(request));
+    public ResponseEntity<?> handleMessage(@RequestBody WebhookEvolutionParameter request) {
+        if (request == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        WebhookEvolution webhookEvolution = request.toModel();
+        this.whatsAppService.handleMessage(webhookEvolution);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
