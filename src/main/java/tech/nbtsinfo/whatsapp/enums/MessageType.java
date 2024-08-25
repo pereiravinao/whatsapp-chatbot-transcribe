64d2a3d32cf2d0aa0ac892ecaff3d74fa5c31142
Vinicius Norberto <vpereira.norberto@gmail.com>
2024-08-25 17:29:55 -0300

    Adiciona Enum para tipar as mensagens recebidas

---
+++ src/main/java/tech/nbtsinfo/whatsapp/enums/MessageType.java
@@ -0,0 +1,28 @@
+package tech.nbtsinfo.whatsapp.enums;
+
+public enum MessageType {
+    TEXT("text"),
+    IMAGE("imageMessage"),
+    VIDEO("videoMessage"),
+    AUDIO("audioMessage"),
+    DOCUMENT("documentMessage");
+
+    private String value;
+
+    MessageType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static MessageType fromValue(String value) {
+        for (MessageType messageType : MessageType.values()) {
+            if (messageType.getValue().equals(value)) {
+                return messageType;
+            }
+        }
+        return null;
+    }
+}
